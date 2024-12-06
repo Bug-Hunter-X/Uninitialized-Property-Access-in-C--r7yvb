562dd41030f813f33cdbfcd0703c20d7511b366a
BugHunter-X <askzenai@gmail.com>
2024-12-06 03:44:05 +0100

    Add bugSolution.cs file

---
+++ bugSolution.cs
@@ -0,0 +1,15 @@
+public class ExampleClass
+{
+    public int MyProperty { get; set; }
+
+    // Constructor to initialize the property
+    public ExampleClass(int initialValue)
+    {
+        MyProperty = initialValue;
+    }
+
+    public void MyMethod()
+    {
+        Console.WriteLine(MyProperty); // Now the value will be reliably set
+    }
+}
